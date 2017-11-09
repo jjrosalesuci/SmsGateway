c55071f4149e9a0547294c95304b45dab5bd7502
Alberto Antonio Ferral Sainz <alberto@cmkx.icrt.cu>
2017-11-09 18:15:47 -0500

    Terminado metodos get, post y put

--- apirestful.sql
+++ apirestful.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-11-2017 a las 18:36:36
+-- Tiempo de generación: 10-11-2017 a las 00:13:22
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 5.6.24
 
@@ -32,9 +32,18 @@ CREATE TABLE `sms` (
   `message` varchar(255) NOT NULL,
   `from_` varchar(255) NOT NULL,
   `status` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `sms`
+--
+
+INSERT INTO `sms` (`id`, `phone_no`, `message`, `from_`, `status`, `created_at`, `updated_at`) VALUES
+(1, '52999698', 'Hola', 'Pepe', 0, '2017-11-09 00:00:00', '2017-11-09 00:00:00'),
+(2, '57285585', 'Estoy aqui.', 'Albert', 0, '2017-11-09 00:00:00', '2017-11-09 00:00:00'),
+(8, '888888888', 'asdasdfafasfadsfasf', 'no te joder', 0, '2017-11-09 16:34:12', '2017-11-09 18:10:44');
 
 --
 -- Índices para tablas volcadas
@@ -54,7 +63,7 @@ ALTER TABLE `sms`
 -- AUTO_INCREMENT de la tabla `sms`
 --
 ALTER TABLE `sms`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
