aa75239809d2a331ce624d9528919c093405d68e
Juan <jjrosalesuci@gmail.com>
2017-11-27 11:56:49 -0300

    Refator

--- app/app.js
+++ app/app.js
@@ -1,11 +1,26 @@
 /*
  * This file is generated and updated by Sencha Cmd. You can edit this file as
- * needed for your application.
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
  */
 Ext.application({
-    name: 'MyApp',
+    name: 'smsfront',
 
-    launch: function () {
-        Ext.Msg.alert('Hello Ext JS', 'Hello! Welcome to Ext JS.');
-    }
+    extend: 'smsfront.Application',
+
+    requires: [
+        'smsfront.view.main.Main'
+    ],
+
+    // The name of the initial view to create. With the classic toolkit this class
+    // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
+    // modern toolkit, the main view will be added to the Viewport.
+    //
+    //mainView: 'smsfront.view.login.Login'
+
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to smsfront.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
 });
