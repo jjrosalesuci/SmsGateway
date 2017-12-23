f18343d4cee6bfe4726dc17ff9b4da9c5a1b9a33
Alberto Antonio Ferral Sainz <alberto@cmkx.icrt.cu>
2017-12-23 18:18:04 -0500

    Dashboard update

--- web-app/src/component/navigation/navigation.ts
+++ web-app/src/component/navigation/navigation.ts
@@ -5,6 +5,7 @@ import {Component}         from '@angular/core';
 @Component({
     selector    : 'navigation',
     templateUrl : './navigation.html',
+    styleUrls: ['./navigation.scss']
 })
 export class NavigationComponent { 
     public isCollapsed: boolean = true;
--- web-app/src/component/page-admin/page-adminhome.component.ts
+++ web-app/src/component/page-admin/page-adminhome.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    templateUrl: './page-adminhome.html'
+    templateUrl: './page-adminhome.html',
+    styleUrls:['./page-adminhome.scss']
 })
 export class AdminHomeComponent implements OnInit {
     constructor() { }
